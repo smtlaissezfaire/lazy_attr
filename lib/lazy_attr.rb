23173528de5662699586271337ff86eed317f2aa
Scott Taylor <scott@railsnewbie.com>
2009-02-23 00:22:11 -0500

    Add lazy_attr

---
+++ lib/lazy_attr.rb
@@ -0,0 +1,18 @@
+module LazyAttr
+  def lazy_attr_reader(method_name, default_value)
+    ivar_name = "@#{method_name}"
+
+    define_method method_name do
+      if ivar = instance_variable_get(ivar_name)
+        ivar
+      else
+        instance_variable_set(ivar_name, default_value.call)
+      end
+    end
+  end
+
+  def lazy_attr_accessor(method_name, default_value)
+    attr_writer(method_name)
+    lazy_attr_reader(method_name, default_value)
+  end
+end
---
+++ spec/lazy_attr_spec.rb
@@ -0,0 +1,74 @@
+require File.dirname(__FILE__) + "/spec_helper"
+
+describe LazyAttr do
+  def new_class(&block)
+    klass = Class.new do
+      extend LazyAttr
+    end
+    klass.class_eval(&block)
+    klass
+  end
+
+  describe "lazy attr reader" do
+    it "should define a lazy attr reader which sets the value on access" do
+      klass = new_class do
+        lazy_attr_reader :foo, lambda { [] }
+      end
+
+      klass.new.foo.should == []
+    end
+
+    it "should use the correct value for the reader" do
+      klass = new_class do
+        lazy_attr_reader :foo, lambda { "foobar" }
+      end
+
+      klass.new.foo.should == "foobar"
+    end
+
+    it "should use the real value if given" do
+      klass = new_class do
+        lazy_attr_reader :foo, lambda { "foobar" }
+        attr_writer :foo
+      end
+
+      obj = klass.new
+
+      obj.foo = "bar"
+      obj.foo.should == "bar"
+    end
+
+    it "should set the ivar on the first access" do
+      klass = new_class do
+        lazy_attr_reader :foo, lambda { "foobar" }
+      end
+
+      obj = klass.new
+
+      obj.foo
+      obj.instance_variable_get("@foo").should == "foobar"
+    end
+  end
+
+  describe "lazy_attr_accessor" do
+    it "should setup a normal writer" do
+      klass = new_class do
+        lazy_attr_accessor :foo, lambda { nil }
+      end
+
+      obj = klass.new
+
+      obj.foo = "bar"
+      obj.foo.should == "bar"
+    end
+
+    it "should setup a lazy_attr_reader" do
+      klass = new_class do
+        lazy_attr_accessor :foo, lambda { "baz" }
+      end
+
+      obj = klass.new
+      obj.foo.should == "baz"
+    end
+  end
+end
